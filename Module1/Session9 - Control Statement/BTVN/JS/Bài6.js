b45fc7b1688ca3ee74a4beaae8f5831e0c4a0f6f
Quý Đạo <tq.dao0802@gmail.com>
2024-08-01 23:35:09 +0900

    update

--- Module1/Session9 - Control Statement/BTVN/JS/Bài6.js
+++ Module1/Session9 - Control Statement/BTVN/JS/Bài6.js
@@ -1,12 +1,17 @@
 let inputNum = +prompt("Mời nhập một số");
 let result = 1;
 document.write(`Giai thừa của ${inputNum} là `);
-for (let i = 1; i <= inputNum; i++) {
-    if (i < inputNum) {
-        document.write(`${i} * `);
-    } else {
-        document.write(`${i} `);
+if (inputNum == 0 || inputNum == 1) {
+    document.write(`1 `);
+} else {
+    for (let i = 1; i <= inputNum; i++) {
+        if (i < inputNum) {
+            document.write(`${i} * `);
+        } else {
+            document.write(`${i} `);
+        }
+        result *= i;
     }
-    result *= i;
+    document.write(`= ${result}`);
 }
-document.write(`= ${result}`);
+
