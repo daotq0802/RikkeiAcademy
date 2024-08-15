77ac927569bbd1f237c7feca14d276c47fc41338
Quý Đạo <tq.dao0802@gmail.com>
2024-08-15 14:51:31 +0900

    update Session 13

--- Module1/Session13 - Object/BTVN/JS/Bài10.js
+++ Module1/Session13 - Object/BTVN/JS/Bài10.js
@@ -2,6 +2,8 @@ const review = document.querySelector('.listProduct pre')
 const cartList = document.querySelector('.cartList pre')
 const sumPrice = document.querySelector('.sumPrice pre')
 const numberPattern = /\B(?=(\d{3})+(?!\d))/g;
+const PRODUCT_STORAGE = 'ListProduct'
+const CART_STORAGE = 'ShoppingCart'
 class Product {
     constructor(name, price, quantity) {
         this.name = name;
@@ -47,23 +49,17 @@ function addToCart(productName, price, quantity) {
     if (list == null) {
         const shoppingCart = [];
         shoppingCart.push(new Product(productName, price, quantity));
-        localStorage.setItem('ShoppingCart', JSON.stringify(shoppingCart))
-        displayCart()
-        calculateTotal()
+        addToLocalStorage(CART_STORAGE, shoppingCart)
     } else {
         let index = checkInCart(productName)
         switch (index) {
             case -1:
                 list.push(new Product(productName, price, quantity));
-                localStorage.setItem('ShoppingCart', JSON.stringify(list))
-                displayCart()
-                calculateTotal()
+                addToLocalStorage(CART_STORAGE, list)
                 break;
             case index:
                 list[index].quantity++
-                localStorage.setItem('ShoppingCart', JSON.stringify(list))
-                displayCart()
-                calculateTotal()
+                addToLocalStorage(CART_STORAGE, list)
                 break;
         }
     }
@@ -72,41 +68,73 @@ function addToCart(productName, price, quantity) {
 function displayCart() {
     const list = JSON.parse(localStorage.getItem('ShoppingCart'));
     let stringList = ''
-    list.forEach(element => {
-        for (const key in element) {
-            let textKey = key.split('').join('')
-            textKey = textKey[0].toUpperCase() + textKey.slice(1)
-            stringList += `<div>${textKey}: ${element[key]}</div>`
+    if (list != null) {
+        document.querySelector('.cartList button').style.display = 'block'
+        list.forEach(element => {
+            for (const key in element) {
+                let textKey = key.split('').join('')
+                textKey = textKey[0].toUpperCase() + textKey.slice(1)
+                stringList += `<div>${textKey}: ${element[key]}</div>`
+            }
+            stringList += `<div><button onclick="removeFromCart('${element.name}')">Delete</button></div><br>`
+        })
+        cartList.innerHTML = stringList
+        calculateTotal()
+    } else {
+        emptyList()
+        try {
+            if (list.length == 0) {
+                emptyList()
+            }
+        } catch (error) {
+            cartList.innerHTML = `Dont Have Any Product In Cart`
         }
-        stringList += `<div><button onclick="removeFromCart('${element.name}')">Delete</button></div><br>`
-    })
-    cartList.innerHTML = stringList
+
+    }
+}
+
+function emptyList() {
+    document.querySelector('.cartList button').style.display = 'none'
+    document.querySelector('.sumPrice').style.display = 'none'
 }
 
 function removeFromCart(productName) {
     let list = JSON.parse(localStorage.getItem('ShoppingCart'))
     let index = checkInCart(productName)
-    if (index != -1) {
-        list.splice(index, 1)
-        localStorage.setItem('ShoppingCart', JSON.stringify(list))
-        displayCart()
+    switch (index) {
+        case index:
+            list.splice(index, 1)
+            addToLocalStorage(CART_STORAGE, list)
+            break;
     }
-    if (list.length < 1) {
-        document.querySelector('.cartList').style.display = 'none'
+    if (list.length == 0) {
+        cartList.innerHTML = `Dont Have Any Product In Cart`
+        document.querySelector('.cartList button').style.display = 'none'
+        document.querySelector('.sumPrice').style.display = 'none'
     }
-    calculateTotal()
 }
 function clearCart() {
     localStorage.removeItem('ShoppingCart')
-    document.querySelector('.cartList').style.display = 'none'
-    sumPrice.innerHTML = `Total: 0`
+    displayCart()
 }
 
 function calculateTotal() {
+    document.querySelector('.sumPrice').style.display = 'block'
+
     let list = JSON.parse(localStorage.getItem('ShoppingCart'))
     let sum = 0
+    let total = 0
     list.forEach(element => {
-        sum += element.price * element.quantity
+        sum = element.price * element.quantity
+        element.sum = sum.toString().replace(numberPattern, ",")
+        total += sum
     })
-    sumPrice.innerHTML = `Total: ${sum.toString().replace(numberPattern, ",")}`
+    localStorage.setItem(CART_STORAGE, JSON.stringify(list))
+    sumPrice.innerHTML = `Total: ${total.toString().replace(numberPattern, ",")}`
+}
+
+function addToLocalStorage(storageName, list) {
+    localStorage.setItem(storageName, JSON.stringify(list))
+    calculateTotal()
+    displayCart()
 }
